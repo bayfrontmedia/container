5748c57cac54daf0591a4ddb855c4be9eac0fc1f
John Robinson <john@bayfrontmedia.com>
2023-01-20 14:32:39 -0500

    v2.0 rewrite

--- src/Container.php
+++ src/Container.php
@@ -1,251 +1,41 @@
 <?php
 
-/**
- * @package container
- * @link https://github.com/bayfrontmedia/container
- * @author John Robinson <john@bayfrontmedia.com>
- * @copyright 2020 Bayfront Media
- */
-
 namespace Bayfront\Container;
 
+use Closure;
 use Psr\Container\ContainerInterface;
 use ReflectionClass;
 use ReflectionException;
+use ReflectionMethod;
 
-/*
- * PSR-11 valid Container Interface
- *
- * See: https://www.php-fig.org/psr/psr-11/
- * See: https://github.com/php-fig/container
- */
 
 class Container implements ContainerInterface
 {
 
-    private static $instances = [];
+    protected $aliases = [];
+    protected $entries = [];
 
     /**
-     * Finds and returns an entry in the container by its identifier
+     * Resolve dependencies.
      *
-     * NOTE:
-     *
-     * Removed return type "object" declaration, as this does not allow
-     * specific class name type hinting/return type declarations
-     * where this method is used.
-     *
-     * @param string $id
-     *
-     * @return mixed
-     *
+     * @throws ContainerException
      * @throws NotFoundException
      */
 
-    public function get($id)
-    {
-
-        if (!$this->has($id)) {
-            throw new NotFoundException($id);
-        }
-
-        return self::$instances[$id];
-
-    }
-
-    /**
-     * Returns an array containing all the IDs which currently exist in the container
-     *
-     * @return array
-     */
-
-    public function getContents(): array
-    {
-        $contents = array_keys(self::$instances);
-
-        sort($contents);
-
-        return $contents;
-    }
-
-    /**
-     * Checks if the container can return an entry for the given identifier
-     *
-     * @param string $id
-     *
-     * @return bool
-     */
-
-    public function has($id): bool
-    {
-        return isset (self::$instances[$id]);
-    }
-
-    /**
-     * Saves a preexisting class instance into the container identified by $id.
-     *
-     * If another entry exists in the container with the same `$id`, it will be overwritten.
-     *
-     * Saving a class instance to the container using its namespaced name as the `$id` will allow it
-     * to be used by the container whenever another class requires it as a dependency.
-     *
-     * @param string $id
-     * @param object $object
-     *
-     * @return self
-     */
-
-    public function put(string $id, object $object): self
-    {
-        self::$instances[$id] = $object;
-        return $this;
-    }
-
-    /**
-     * Creates a class instance using create() and saves it into the container identified by $id.
-     * An instance of the class will be returned.
-     *
-     * If another entry exists in the container with the same $id, it will be overwritten
-     *
-     * Saving a class instance to the container using its namespaced name as the `$id` will allow it
-     * to be used by the container whenever another class requires it as a dependency.
-     *
-     * NOTE:
-     * See get() regarding removal of return type "object" declaration.
-     *
-     * @param string $id
-     * @param string $class (Fully namespaced class name)
-     * @param array $params (Named parameters to pass to the class constructor)
-     *
-     * @return mixed
-     *
-     * @throws ContainerException
-     */
-
-    public function set(string $id, string $class, array $params = [])
-    {
-
-        self::$instances[$id] = $this->create($class, $params);
-
-        return self::$instances[$id];
-
-    }
-
-    /**
-     * Creates a class instance using dependency injection.
-     * An instance of the class will be returned, but not saved in the container.
-     *
-     * If this namespaced class already exists in the container as an $id,
-     * the instance existing in the container will be returned by default.
-     *
-     * NOTE:
-     * See get() regarding removal of return type "object" declaration.
-     *
-     * @param string $class (Fully namespaced class name)
-     * @param array $params (Named parameters to pass to the class constructor)
-     * @param bool $force_unique
-     *
-     * (Force return a new class instance by ignoring if it already exists in the container)
-     *
-     * @return mixed
-     *
-     * @throws ContainerException
-     */
-
-    public function create(string $class, array $params = [], bool $force_unique = false)
-    {
-
-        /*
-         * First, see if an instance of this class exists in the container
-         */
-
-        if ($this->has($class) && false === $force_unique) {
-
-            try {
-
-                return $this->get($class);
-
-            } catch (NotFoundException $e) {
-
-                throw new ContainerException('Error resolving class from container: ' . $class, 0, $e);
-
-            }
-
-        }
-
-        /*
-         * If not, try to create one
-         */
-
-        try {
-
-            $reflection = new ReflectionClass($class);
-
-        } catch (ReflectionException $e) {
-
-            throw new ContainerException('Unable to locate class: ' . $class, 0, $e);
-
-        }
-
-        if (!$reflection->isInstantiable()) {
-
-            throw new ContainerException('Unable to instantiate class: ' . $class);
-
-        }
-
-        // Constructor
-
-        $constructor = $reflection->getConstructor();
-
-        /*
-         * If class has no constructor (no dependencies)
-         * it can be instantiated now
-         */
-
-        if (NULL === $constructor) { // No constructor
-
-            return new $class; // Class instance
-
-        }
-
-        /*
-         * Class has constructor, get and pass its dependencies
-         * (The class arguments required in its constructor)
-         */
-
-        $dependencies = $this->_getDependencies($class, $constructor->getParameters(), $params);
-
-        /*
-         * Create the class instance using the identified dependencies
-         */
-
-        return $reflection->newInstanceArgs($dependencies); // Class instance
-
-    }
-
-    /**
-     * Resolve the class dependencies
-     *
-     * @param string $class (Fully namespaced class name)
-     * @param array $class_params
-     * @param array $given_params
-     *
-     * @return array
-     *
-     * @throws ContainerException
-     */
-
-    private function _getDependencies(string $class, array $class_params, array $given_params): array
+    protected function resolveDependencies(ReflectionMethod $constructor, array $params = []): array
     {
 
         $return = [];
 
-        foreach ($class_params as $parameter) {
+        $parameters = $constructor->getParameters();
 
-            // $parameter->name = Argument name assigned to the parameter within the class constructor
+        foreach ($parameters as $parameter) {
 
-            if (isset($given_params[$parameter->name])) { // If a value was given for this parameter
+            // If a value was given for this parameter
 
-                $return[$parameter->name] = $given_params[$parameter->name];
+            if (isset($params[$parameter->name])) {
+
+                $return[$parameter->name] = $params[$parameter->name];
 
                 continue; // Continue to the next parameter
 
@@ -253,9 +43,18 @@ class Container implements ContainerInterface
 
             // A value was not passed. Attempt to resolve it
 
-            $dependency = $parameter->getClass();
+            /*
+             * Depreciated:
+             * $dependency = $parameter->getClass();
+             */
 
-            if (NULL === $dependency) { // If not a class object
+            try {
+                $dependency = $parameter->getType() && !$parameter->getType()->isBuiltin() ? new ReflectionClass($parameter->getType()->getName()) : null;
+            } catch (ReflectionException $e) {
+                throw new ContainerException('Unable to determine parameter (' . $parameter . ') for class: ' . $constructor->getDeclaringClass());
+            }
+
+            if (null === $dependency) { // If not a class
 
                 if ($parameter->isDefaultValueAvailable()) { // Is a default value available
 
@@ -265,53 +64,28 @@ class Container implements ContainerInterface
 
                 }
 
-                /*
-                 * Parameter is not a class, a value was not given, and the class
-                 * has no default value defined
-                 */
+                // A value was not given for this parameter, it is not a class, and a default value was not given
 
-                throw new ContainerException('Unable to resolve parameter (' . $parameter . ') for class: ' . $class);
+                throw new ContainerException('Unable to resolve parameter (' . $parameter . ') for class: ' . $constructor->getDeclaringClass());
 
             }
 
             /*
-             * Parameter is a class, and its instance was not given.
+             * Parameter is a class, and its value was not given.
              * Attempt to resolve it.
              *
              * $dependency->name = Namespaced class
              */
 
-            /*
-             * First, see if an instance of this class exists in the container
-             */
+            if ($this->has($dependency->name)) { // First, see if an instance of this class exists in the container
 
-            if ($this->has($dependency->name)) {
+                $return[$parameter->name] = $this->get($dependency->name); // Class instance
 
-                try {
+            } else { // No instance in the container - attempt to resolve
 
-                    $return[$parameter->name] = $this->get($dependency->name); // Class instance
-
-                } catch (NotFoundException $e) {
-
-                    throw new ContainerException('Error resolving dependent class from container: ' . $dependency->name, 0, $e);
-
-                }
+                $return[$parameter->name] = $this->resolve($dependency->name, $params);
 
             }
-
-            try {
-
-                $resolved = $this->create($dependency->name); // Returns object
-
-            } catch (ContainerException $e) {
-
-                throw new ContainerException('Unable to resolve object parameter (' . $parameter . ') for class: ' . $class, 0, $e);
-
-            }
-
-            $return[$parameter->name] = $resolved; // Class instance
-
-            continue; // Continue to the next parameter
 
         }
 
@@ -320,28 +94,195 @@ class Container implements ContainerInterface
     }
 
     /**
-     * Remove class instance from the container, if existing
+     * Set an entry into the container.
      *
      * @param string $id
-     *
-     * @return bool (Returns TRUE if class existed in the container before being removed)
+     * @param $value
+     * @param bool $overwrite (If false, a ContainerException is thrown if an entry with the same ID already exists)
+     * @return void
+     * @throws ContainerException
      */
 
-    public function forget($id): bool
+    public function set(string $id, $value, bool $overwrite = false): void
     {
 
-        if ($this->has($id)) {
-
-            self::$instances[$id] = NULL;
-
-            unset(self::$instances[$id]);
-
-            return true;
-
+        if ($overwrite === false && $this->has($id)) {
+            throw new ContainerException('Unable to set entry: ID (' . $id . ') already exists');
         }
 
-        return false;
+        $this->entries[$id] = $value;
 
+    }
+
+    /**
+     * Returns an array of all ID's existing in the container.
+     *
+     * @return array
+     */
+
+    public function getEntries(): array
+    {
+        return array_keys($this->entries);
+    }
+
+    /**
+     * Get an entry from the container.
+     *
+     * @param string $id
+     * @return mixed
+     * @throws NotFoundException
+     */
+
+    public function get(string $id)
+    {
+
+        // Check alias
+
+        if ($this->hasAlias($id) && $this->has($this->aliases[$id])) {
+            return $this->get($this->aliases[$id]);
+        }
+
+        // No alias
+
+        if (!$this->has($id)) {
+            throw new NotFoundException('Unable to get entry: ID (' . $id . ') does not exist');
+        }
+
+        if (!$this->entries[$id] instanceof Closure) { // Already resolved
+            return $this->entries[$id];
+        }
+
+        $this->entries[$id] = $this->entries[$id]($this); // Resolve and save
+
+        return $this->entries[$id];
+
+    }
+
+    /**
+     * Resolves a class instance using dependency injection for entries which exist in the container.
+     *
+     * @param string $class
+     * @param array $params (Additional parameters to pass to the class constructor)
+     * @return object
+     * @throws ContainerException
+     * @throws NotFoundException
+     */
+
+    public function resolve(string $class, array $params = []): object
+    {
+
+        try {
+            $reflection = new ReflectionClass($class);
+        } catch (ReflectionException $e) {
+            throw new ContainerException('Unable to resolve class: class (' . $class . ') does not exist', 0, $e);
+        }
+
+        $constructor = $reflection->getConstructor();
+
+        if (!$constructor) {
+            return new $class;
+        }
+
+        $dependencies = $this->resolveDependencies($constructor, $params);
+
+        try {
+            return $reflection->newInstanceArgs($dependencies);
+        } catch (ReflectionException $e) {
+            throw new ContainerException('Unable to resolve class: ' . $class, 0, $e);
+        }
+
+    }
+
+    /**
+     * Does entry exist in the container?
+     *
+     * @inheritDoc
+     */
+
+    public function has(string $id): bool
+    {
+
+        // Check alias
+
+        if ($this->hasAlias($id) && $this->has($this->aliases[$id])) {
+            return true;
+        }
+
+        return isset($this->entries[$id]);
+    }
+
+    /**
+     * Remove entry from container.
+     *
+     * @param string $id
+     * @return void
+     */
+
+    public function remove(string $id): void
+    {
+
+        if (isset($this->entries[$id])) {
+            unset($this->entries[$id]);
+        }
+
+    }
+
+    /**
+     * Set an alias for a given ID.
+     *
+     * @param string $alias
+     * @param string $id
+     * @param bool $overwrite (If false, a ContainerException is thrown if an alias with the same name already exists)
+     * @return void
+     * @throws ContainerException
+     */
+
+    public function setAlias(string $alias, string $id, bool $overwrite = false): void
+    {
+
+        if ($overwrite === false && $this->hasAlias($alias)) {
+            throw new ContainerException('Unable to set alias: Alias (' . $alias . ') already exists');
+        }
+
+        $this->aliases[$alias] = $id;
+
+    }
+
+    /**
+     * Returns an array of all existing aliases.
+     *
+     * @return array
+     */
+
+    public function getAliases(): array
+    {
+        return array_keys($this->aliases);
+    }
+
+    /**
+     * Does alias exist?
+     *
+     * @param string $alias
+     * @return bool
+     */
+
+    public function hasAlias(string $alias): bool
+    {
+        return isset($this->aliases[$alias]);
+    }
+
+    /**
+     * Remove alias.
+     *
+     * @param string $alias
+     * @return void
+     */
+
+    public function removeAlias(string $alias): void
+    {
+        if (isset($this->aliases[$alias])) {
+            unset($this->aliases[$alias]);
+        }
     }
 
 }
